4164dbb4b7e74d43f6c0ea4cfb317823635cc969
k.solovev <k.solovev.work@gmail.com>
2019-08-15 22:13:20 +0300

    исправил ошибки в остальных браузерах

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,13 +1,19 @@
 var popupSuccess = document.querySelector(".popup-success");
 var popupError = document.querySelector(".popup-error");
 var feedbackBtn = document.querySelector('.feedback__btn');
+var feedbackInputName = document.querySelector('.feedback__input--name');
+var feedbackInputSecondName = document.querySelector('.feedback__input--second-name');
+var feedbackInputThirdName = document.querySelector('.feedback__input--third-name');
 
-popupSuccess.classList.remove("popup-success--open");
-popupError.classList.remove("popup-error--open");
+popupSuccess.classList.remove("popup-success--opened");
+popupError.classList.remove("popup-error--opened");
 
 feedbackBtn.addEventListener("click", function() {
-  if(popupSuccess.classList.contains("popup-success--closed")) {
-      popupSuccess.classList.remove("popup-success--closed");
-      popupSuccess.classList.add("popup-success--opened");
+  if (feedbackInputName.value === '') {
+    popupError.classList.add("popup-error--opened");
+  } else if (feedbackInputSecondName.value === '') {
+    popupError.classList.add("popup-error--opened");
+  } else {
+    popupError.classList.add("popup-success--opened");
   }
 });
