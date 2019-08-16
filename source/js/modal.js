c9fcfaeb5b7c1f8ee8ffe9afd3dd5f482c0ba1da
k.solovev <k.solovev.work@gmail.com>
2019-08-16 18:07:24 +0300

    добавил работу модальных окон, поправил карту

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,19 +1,41 @@
 var popupSuccess = document.querySelector(".popup-success");
 var popupError = document.querySelector(".popup-error");
 var feedbackBtn = document.querySelector('.feedback__btn');
-var feedbackInputName = document.querySelector('.feedback__input--name');
-var feedbackInputSecondName = document.querySelector('.feedback__input--second-name');
-var feedbackInputThirdName = document.querySelector('.feedback__input--third-name');
+var errorBtn = document.querySelector('.popup-error__btn');
+var successBtn = document.querySelector('.popup-success__btn');
+var inputName = document.querySelector('.feedback__input--name');
+var inputSecondName = document.querySelector('.feedback__input--second-name');
+var inputThirdName = document.querySelector('.feedback__input--third-name');
+var inputPhone = document.querySelector('.feedback__input--tel');
+var inputEmail = document.querySelector('.feedback__input--email');
 
 popupSuccess.classList.remove("popup-success--opened");
 popupError.classList.remove("popup-error--opened");
 
+// проверка полей
 feedbackBtn.addEventListener("click", function() {
-  if (feedbackInputName.value === '') {
+  if (inputName.value === '') {
     popupError.classList.add("popup-error--opened");
-  } else if (feedbackInputSecondName.value === '') {
+  } else if (inputSecondName.value === '') {
+    popupError.classList.add("popup-error--opened");
+  } else if (inputThirdName.value === '') {
+    popupError.classList.add("popup-error--opened");
+  } else if (inputPhone.value === '') {
+    popupError.classList.add("popup-error--opened");
+  } else if (inputEmail.value === '') {
     popupError.classList.add("popup-error--opened");
   } else {
-    popupError.classList.add("popup-success--opened");
+    popupSuccess.classList.add("popup-success--opened");
   }
 });
+
+//закрытие модальных окон
+errorBtn.addEventListener("click", function () {
+  popupError.classList.remove("popup-error--opened");
+  popupError.classList.add("popup-error--closed");
+})
+
+successBtn.addEventListener("click", function () {
+  popupSuccess.classList.remove("popup-success--opened");
+  popupSuccess.classList.add("popup-success--closed");
+})
